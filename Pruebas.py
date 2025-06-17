cae0ea99e780eddc6f28ab6efbe79dae5ec39011
OmarTerreros <yairterrerosmtz@gmail.com>
2025-06-17 23:13:35 +0000

    Clon de archivo

---
+++ Pruebas.py
@@ -0,0 +1,19 @@
+#Definiendo funciones
+
+valor_numerico= int(input("Agrega un valor:"))
+valor_numerico2= int(input("Agrega un valor:"))
+
+def sumar(valor_numerico,valor_numerico2):
+    agregar= valor_numerico + valor_numerico2
+    return agregar
+print(sumar(valor_numerico,valor_numerico2))
+
+def resta(valor_numerico,valor_numerico2):
+    quitar= valor_numerico - valor_numerico2
+    return quitar
+print(resta(valor_numerico,valor_numerico2))
+
+def multiplicar(valor_numerico,valor_numerico2):
+    multiplica= valor_numerico * valor_numerico2
+    return multiplica
+print(multiplicar(valor_numerico,valor_numerico2))
